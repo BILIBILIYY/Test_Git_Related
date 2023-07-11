a49e282ab98c8d1a699685d70ca92f1ed98a4d87
Bilibili_yy <290005031@qq.com>
2023-07-11 10:56:26 +0800

    first commit

---
+++ src/main/java/com/xiaomi/yangyi26/test/App.java
@@ -0,0 +1,13 @@
+package com.xiaomi.yangyi26.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
